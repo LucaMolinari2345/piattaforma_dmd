611d53dfc279c0ec085fbe5bb0aabe14c7ff0e35
Luca Molinari <lucamolinari@Air-di-Luca.home>
2024-09-10 12:24:20 +0200

    Updates

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonkey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonkey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export default createClient(supabaseUrl, supabaseAnonkey);
